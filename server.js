07d680dcb4a320ac82975eeb20467f4c5f8dc2f7
Fejaan Rathore <your-email@example.com>
2026-06-14 11:27:54 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+
+let notes = [];
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+
+    const note = {
+        title: req.body.title,
+        content: req.body.content
+    };
+
+    notes.push(note);
+
+    res.json({
+        message: "Note Saved Successfully"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server Running at http://localhost:3000");
+});
